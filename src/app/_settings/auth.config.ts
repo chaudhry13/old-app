5a76e5a34df6623ee1a8046157b3b3d843770f81
Mads Smed <ms@humanrisks.com>
2019-12-02 11:28:17 +0100

    Add local dev authConfig

--- src/app/_settings/auth.config.ts
+++ src/app/_settings/auth.config.ts
@@ -9,3 +9,12 @@ export const authConfig: AuthConfig = {
 	oidc: false,
 	scope: "api"
 };
+
+export const authConfigLocal: AuthConfig = {
+	issuer: "https://localhost:5001",
+	redirectUri: "http://localhost:8100/callback",
+	logoutUrl: "https://localhost:5001/account/logout",
+	clientId: "ionic",
+	oidc: false,
+	scope: "api"
+};
