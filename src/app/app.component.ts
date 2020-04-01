c96ed9c82a79cad666bd60874d36937760db4e17
Mads Smed <mthoma18@student.aau.dk>
2020-04-01 10:19:09 +0200

    Cleanup of imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
-import { Component, isDevMode } from "@angular/core";
-
+import { Component } from "@angular/core";
 import { Platform } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
-import { OAuthService, NullValidationHandler, OAuthErrorEvent, JwksValidationHandler, AuthConfig } from "angular-oauth2-oidc";
-import { AppConfig } from "./_settings/auth.config";
-import { Router } from "@angular/router";
 import { AppConfigService } from './_services/auth-config.service';
 
 @Component({
