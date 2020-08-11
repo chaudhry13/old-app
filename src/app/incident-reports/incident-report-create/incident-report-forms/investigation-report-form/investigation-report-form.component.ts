d08b85fd205dd065e382f71a9a68344bda3b0d75
Mads Smed <mads.th@hotmail.com>
2020-08-11 10:10:25 +0200

    Added Investigation report form

---
+++ src/app/incident-reports/incident-report-create/incident-report-forms/investigation-report-form/investigation-report-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'investigation-report-form',
+  templateUrl: './investigation-report-form.component.html',
+  styleUrls: ['./investigation-report-form.component.scss']
+})
+export class InvestigationReportFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
