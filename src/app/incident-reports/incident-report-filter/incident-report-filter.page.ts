0efe3a64280c1b6e2fb0a22715cb026ed1b33a89
Mads Smed <ms@humanrisks.com>
2020-08-28 17:30:21 +0200

    Added new filterform and default filter now has Types

--- src/app/incident-reports/incident-report-filter/incident-report-filter.page.ts
+++ src/app/incident-reports/incident-report-filter/incident-report-filter.page.ts
@@ -14,9 +14,7 @@ import { IncidentCategoryService } from '../../_services/incident-category.servi
   styleUrls: ["incident-report-filter.page.scss"]
 })
 export class IncidentReportFilterPage implements OnInit {
-  @Input() form: FormGroup;
-
-  filterForm: FormGroup;
+  @Input() filterForm: FormGroup;
 
   divisions: Division[];
   countries: Country[];
@@ -31,6 +29,8 @@ export class IncidentReportFilterPage implements OnInit {
       startDate: [new Date().toJSON()],
       endDate: [new Date().toJSON()],
       incidentCategoryIds: [""],
+      incidentTypeIds: [""],
+      description: [null],
       riskLevels: [""],
       divisionIds: [""],
       countryIds: [""],
@@ -39,23 +39,64 @@ export class IncidentReportFilterPage implements OnInit {
       southWestLatitude: [0, Validators.required],
       southWestLongitude: [0, Validators.required],
       northEastLatitude: [0, Validators.required],
-      northEastLongitude: [0, Validators.required]
+      northEastLongitude: [0, Validators.required],
+
+      apparel: [null],
+      approxAgeMax: [null],
+      approxAgeMin: [null],
+      build: [null],
+      gender: [null],
+      heightMax: [null],
+      heightMin: [null],
+      identifyingfeatures: [null],
+      incidentName: [null],
+      obervationName: [null],
+      email: [],
+
+      vehicleDescription: [null],
+      color: [null],
+      colorOther: [null],
+      make: [null],
+      makeOther: [null],
+      model: [null],
+      modelOther: [null],
+      vrm: [null],
+
+      users: [[]],
+      customField1: [null],
+      customField2: [null],
+      customField3: [null],
+      customField4: [null],
+      customField5: [null],
+      customField6: [null],
+      customField7: [null],
     });
   }
 
   ngOnInit() {
+    this.listDivisions();
+    this.listCategories();
+
+    this.setStartEndDate();
+  }
+
+  private listDivisions() {
     this.divisionService.list().then(divisions => {
       this.divisions = divisions;
     });
+  }
 
+  private listCategories() {
     this.incidentCategoryService.list(true).then(categories => {
       this.incidentCategories = categories;
+      this.incidentTypes = this.incidentCategoryService.listIncidentTypes(categories);
+      console.log(this.incidentTypes);
     });
+  }
 
-    this.filterForm = this.form;
-
-    let startDate = this.form.controls.startDate.value;
-    let endDate = this.form.controls.endDate.value;
+  private setStartEndDate() {
+    let startDate = this.filterForm.controls.startDate.value;
+    let endDate = this.filterForm.controls.endDate.value;
 
     this.filterForm.controls.startDate.setValue(startDate);
     this.filterForm.controls.endDate.setValue(endDate);
