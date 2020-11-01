735b14b6399f679de256bd6f9a19093e2603e64e
Mads Smed <ms@humanrisks.com>
2020-11-01 10:42:42 +0100

    Moved places service service

--- src/app/shared/components/location-modal/location-model.spec.ts
+++ src/app/shared/components/location-modal/location-model.spec.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { IonicModule, ModalController } from "@ionic/angular";
 import { RouterTestingModule } from "@angular/router/testing";
 import { LocationModalPage } from "./location-modal.page";
-import { PlacesSearchService } from "../../../core/services/places-search.service";
+import { PlacesSearchService } from "@shared/services/places-search.service";
 
 declare var google: any;
 
