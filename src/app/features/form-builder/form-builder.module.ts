02d090e486c346b4d79365812998d340045a5e48
Kaare Hattel <kaare.hattel@gmail.com>
2022-08-14 23:06:32 +0200

    Update form-builder.module.ts

--- src/app/features/form-builder/form-builder.module.ts
+++ src/app/features/form-builder/form-builder.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {EvalLogicPipe} from "./form-answer/eval-logic.pipe";
 import {FormAnswerComponent} from "./form-answer/form-answer.component";
+import {
+    DropDownBuildingComponentComponent
+} from "./building-components/drop-down-building-component/drop-down-building-component.component";
 
 
 @NgModule({
   declarations: [
       EvalLogicPipe,
     FormAnswerComponent,
+      DropDownBuildingComponentComponent
   ],
   imports: [
       CommonModule
